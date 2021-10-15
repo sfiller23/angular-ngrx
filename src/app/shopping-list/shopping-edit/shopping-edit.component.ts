48e7284d3bb27ef3b577b0904b1b5dd70c925bb8
sfiller23 <sf27lp@gmail.com>
2021-10-15 22:08:30 +0300

    final version

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+
+import { Ingredient } from '../../shared/ingredient.model';
+import * as ShoppingListActions from '../store/shopping-list.actions';
+import * as fromApp from '../../store/app.reducer';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('f') slForm: NgForm;
+  subscription: Subscription;
+  editMode = false;
+  editedItem: Ingredient;
+
+  constructor(
+    private store: Store<fromApp.AppState>
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.store
+      .select('shoppingList')
+      .subscribe(stateData => {
+        const index = stateData.editIndex;
+        if (index > -1) {
+          this.editMode = true;
+          this.editedItem = stateData.ingredients[index];
+          this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+          });
+        } else {
+          this.editMode = false;
+        }
+      });
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if (this.editMode) {
+      // this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      this.store.dispatch(
+        ShoppingListActions.updateIngredient({ingredient: newIngredient})
+      );
+    } else {
+      // this.slService.addIngredient(newIngredient);
+      this.store.dispatch(ShoppingListActions.addIngredient({ingredient: newIngredient}));
+    }
+    this.editMode = false;
+    form.reset();
+  }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+    this.store.dispatch(ShoppingListActions.stopEdit());
+  }
+
+  onDelete() {
+    // this.slService.deleteIngredient(this.editedItemIndex);
+    this.store.dispatch(ShoppingListActions.deleteIngredient());
+    this.onClear();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    this.store.dispatch(ShoppingListActions.stopEdit());
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'AIzaSyBzUqc8WC8-2SAj-3eVMoWBpWlINgBUzKo'
+};
